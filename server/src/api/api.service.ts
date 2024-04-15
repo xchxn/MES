4a392b4b6e19423f880f5f9473cd0c31298f086d
LeeYuChan <dldbcks7758@naver.com>
2024-04-15 20:01:43 +0900

    server.api.update.2

--- server/src/api/api.service.ts
+++ server/src/api/api.service.ts
@@ -10,24 +10,7 @@ export class ApiService {
   ) {}
 
   async updateData(data: ProductInventory): Promise<ProductInventory> {
-    return await this.productInventoryRepository.save(data);
-  }
-
-  async apiView(
-    inventory_id: string,
-    category_name: string,
-    type_name: string,
-    grade_name: string,
-    previous_month_stock: number,
-    previous_month_weight: number,
-    incoming_quantity: number,
-    incoming_weight: number,
-    outgoing_quantity: number,
-    outgoing_weight: number,
-    current_stock: number,
-    current_weight: number,
-  ): Promise<any> {
-    const dataExample = {
+    const {
       inventory_id,
       category_name,
       type_name,
@@ -40,32 +23,30 @@ export class ApiService {
       outgoing_weight,
       current_stock,
       current_weight,
-    };
+    } = data;
 
-    console.log(dataExample);
+    console.log(data);
 
     await this.productInventoryRepository
       .createQueryBuilder()
       .insert()
       .into(ProductInventory)
-      .values([
-        {
-          inventory_id: inventory_id,
-          category_name: category_name,
-          type_name: type_name,
-          grade_name: grade_name,
-          previous_month_stock: previous_month_stock,
-          previous_month_weight: previous_month_weight,
-          incoming_quantity: incoming_quantity,
-          incoming_weight: incoming_weight,
-          outgoing_quantity: outgoing_quantity,
-          outgoing_weight: outgoing_weight,
-          current_stock: current_stock,
-          current_weight: current_weight,
-        },
-      ])
+      .values({
+        inventory_id,
+        category_name,
+        type_name,
+        grade_name,
+        previous_month_stock,
+        previous_month_weight,
+        incoming_quantity,
+        incoming_weight,
+        outgoing_quantity,
+        outgoing_weight,
+        current_stock,
+        current_weight,
+      })
       .execute();
-
-    return dataExample;
+    console.log('check');
+    return this.productInventoryRepository.findOneBy({ inventory_id });
   }
 }
